2478c5558f9bf3563d672b83837916883df1bcc8
Maipham72 <hongmai.pham@student.adelaide.edu.au>
2023-05-20 16:09:54 +0930

    format

--- valid.cpp
+++ valid.cpp
@@ -1,16 +1,13 @@
 #ifndef VALID_H
 #define VALID_H
 #include <iostream>
-#include <string>
 #include <limits>
+#include <string>
 
-
-    bool isNumValid(int data) {
-        return ((data >= 1) && (data <= 4));
-    }
-    void clearInput() {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    }
+bool isNumValid(int data) { return ((data >= 1) && (data <= 4)); }
+void clearInput() {
+  std::cin.clear();
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
 
 #endif
