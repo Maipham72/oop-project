7106d3c6f5adc4e3f7a4ea0b5187af76d2018c05
joanne <joannesu2003@gmail.com>
2023-05-23 11:13:30 +0930

    Removed some tests

--- cardPaymentTest.cpp
+++ cardPaymentTest.cpp
@@ -66,7 +66,7 @@ class CardPaymentTest {
       // Card does not have enough balance
       // Expecting a false boolean variable +
       if (card_payment->pay(amount) == 1) {
-        cout << "Test 1 for card payment failed!" << endl;
+        cout << "Test 5 for card payment failed!" << endl;
       }
     }
      // Test 6
@@ -77,14 +77,14 @@ class CardPaymentTest {
       // Card does not have enough balance
       // Expecting a false boolean variable 
       if (card_payment->pay(amount) == 1) {
-        cout << "Test 1 for card payment failed!" << endl;
+        cout << "Test 6 for card payment failed!" << endl;
       }
     }
   }
 
   // Tests to test for the boundary values for card payment
   void testZeroValue() {
-    // Test 6
+    // Test 7
     {
       float amount = 0;
       float balance = 0;
@@ -92,7 +92,7 @@ class CardPaymentTest {
       // Paying exact amount
       // Expecting a true boolean variable
       if (card_payment->pay(amount) == 0) {
-        cout << "Test 6 for card payment failed!" << endl;
+        cout << "Test 7 for card payment failed!" << endl;
       }
     }
   }
